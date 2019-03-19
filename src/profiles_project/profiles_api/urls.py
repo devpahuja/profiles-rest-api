51c6e1d50118e41f615fc7240dae9ddb685b3a20
dev <devpahuja10@gmail.com>
2019-03-19 12:54:45 +0530

    Added HelloAPIViewset

--- src/profiles_project/profiles_api/urls.py
+++ src/profiles_project/profiles_api/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('view-set', views.HelloAPIViewset, base_name='view-set') 
 
 urlpatterns = [
 	path('hello-view/',views.HelloAPIView.as_view()),
+	path('',include(router.urls)),
 ]
--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import request
+from rest_framework import status
+from . import serializers
+
+from rest_framework import viewsets
 
 # Create your views here.
 class HelloAPIView(APIView):
 
 	''' Test API view.'''
+	
+	serializer_class = serializers.HelloSerializer
 	
 	def get(request, self, format=None):
 	
@@ -19,3 +26,32 @@ class HelloAPIView(APIView):
 		]
 		
 		return Response({'message': "Hello World", 'an_apiview': an_apiview})
+
+	def post(self, request):
+		
+		''' Create a hello message with our name. '''
+		#print(request.data)
+		serializer = serializers.HelloSerializer(data = request.data)
+		
+		if serializer.is_valid():
+			#print(serializer.data)
+			name = serializer.data.get('name')
+			message = 'Hello {0}'.format(name)
+			return Response({'message': message})
+		
+		else:
+			return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+			
+
+class HelloAPIViewset(viewsets.ViewSet):
+
+	''' Test Api Viewset. '''
+	
+	def list(self, request):
+		a_viewset = [
+			'It gives more functionality with less code. ',
+			'It uses actions (LIST, RETRIEVE, CREATE, UPDATE, PARTIAL-UPDATE)',
+		]
+		
+		return Response({'message': 'Hello!', 'a_viewset': a_viewset})
+		
